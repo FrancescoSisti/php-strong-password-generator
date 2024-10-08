d2c283fac1917724805ba34aceec540fde93fcc0
FrancescoSisti <francescosisti61@gmail.com>
2024-10-08 15:46:48 +0200

    milestone 1 - all logic and style in the same file

--- index.php
+++ index.php
@@ -11,10 +11,41 @@
 </head>
 
 <body>
-    <?php include __DIR__ . '/components/functions.php'; ?>
-    <h1 class="text-center">Strong Password Generator</h1>
-    <h2 class="text-center">Password generata: <?php echo generatePassword(12); ?></h2>
+    <?php
+    function generatePassword($length)
+    {
+        $dictionary = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}[]|:;,.?/~';
+        $password = '';
+        for ($i = 0; $i < $length; $i++) {
+            $password = $dictionary[rand(0, strlen($dictionary) - 1)];
+        }
+        return $password;
+    }
 
+    $password = '';
+    $length = isset($_GET['length']) ? intval($_GET['length']) : 0;
+    if ($length > 0) {
+        $password = generatePassword($length);
+    }
+    ?>
+
+    <div class="container mt-5">
+        <h1 class="text-center mb-4">Strong Password Generator</h1>
+
+        <form action="" method="GET" class="mb-4">
+            <div class="mb-3">
+                <label for="length" class="form-label">Lunghezza della password:</label>
+                <input type="number" class="form-control" id="length" name="length" min="1" max="20">
+            </div>
+            <button type="submit" class="btn btn-primary">Genera Password</button>
+        </form>
+
+        <?php if ($password): ?>
+            <div class="alert alert-success">
+                <h2 class="mb-0">Password generata: <?php echo $password; ?></h2>
+            </div>
+        <?php endif; ?>
+    </div>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.bundle.min.js"
         integrity="sha512-7Pi/otdlbbCR+LnW+F7PwFcSDJOuUJB3OxtEHbg4vSMvzvJjde4Po1v4BR9Gdc9aXNUNFVUY+SK51wWT8WF0Gg=="
